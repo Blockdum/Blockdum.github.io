b60987df67c7955764a3104ec5ca8ae85a36d625
Kris Zhang <kriszhang122507@gmail.com>
2021-12-24 19:24:11 +1300

    Update countdown timer

--- Countdown.js
+++ Countdown.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "20 Dec 2021";
+const newYears = "15 Jan 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
