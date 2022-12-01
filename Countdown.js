8427ff5e63eb701274f450ab6c251ebb3d3fcdf3
Kris-Zhang-122507 <kriszhang122507@gmail.com>
2022-12-02 12:01:25 +1300

    update countdown

--- Countdown.js
+++ Countdown.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "30 November 2022";
+const newYears = "15 June 2023";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
